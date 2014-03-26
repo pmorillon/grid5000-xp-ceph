0a686a20435b54195def765e88769bc55257e6ec
Pascal Morillon <pascal.morillon@irisa.fr>
2014-03-26 17:39:28 +0100

    Manage ceph.conf file

--- provision/puppet/modules/xp/manifests/ceph.pp
+++ provision/puppet/modules/xp/manifests/ceph.pp
@@ -4,6 +4,12 @@ class xp::ceph {
   include 'xp::ceph::osd'
   include 'xp::ceph::mon'
 
+  $node_description = hiera_hash('node_description')
+  $osd_devices = $node_description['osd']
+  $fs = hiera('filesystem')
+  $nodes = hiera_array('ceph_nodes')
+  $vlan_id = hiera('vlan')
+
   package {
     ['xfsprogs', 'parted']:
       ensure => installed;
@@ -12,6 +18,15 @@ class xp::ceph {
   file {
     '/srv/ceph':
       tag    => 'ceph_tree';
+  }
+
+  file {
+    '/etc/ceph/ceph.conf':
+      ensure  => file,
+      mode    => '0644',
+      owner   => root,
+      group   => root,
+      content => template('xp/ceph/ceph.conf.erb');
   }
 
 }
